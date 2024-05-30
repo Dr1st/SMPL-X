32b5f9147fe0c4b6ed73306c252491dec84a1b3c
Lee <40255250+Dr1st@users.noreply.github.com>
2024-05-30 18:10:14 +0400

    Update parameters

--- examples/demo.py
+++ examples/demo.py
@@ -16,45 +16,47 @@
 
 
 # Parameters Meaning
-# Beta 0 (Height):
-# Range: -3 to 3
-# Effect: -3 for shorter, 3 for taller
 
-# Beta 1 (Body fat):
-# Range: -3 to 3
-# Effect: -3 for leaner, 3 for more body fat
+# Beta 0 (Overall Body Size):
+# Range: -5 to 5
+# Effect: -5 for fatter, +5 for thinner and smaller
 
-# Beta 2 (Arm length):
-# Range: -3 to 3
-# Effect: -3 for shorter arms, 3 for longer arms
+# Beta 1 (Side Compression and Stretching):
+# Range: 0 to positive values
+# Effect: Positive values make the body fatter
 
-# Beta 3 (Leg length):
-# Range: -3 to 3
-# Effect: -3 for shorter legs, 3 for longer legs
+# Beta 2 (General Body Fat):
+# Range: 0 to positive values
+# Effect: Positive values make the body fatter
 
-# Beta 4 (Torso length):
-# Range: -3 to 3
-# Effect: -3 for shorter torso, 3 for longer torso
+# Beta 3 (Belly Size and Body Reduction):
+# Range: 0 to negative values
+# Effect: Negative values make the belly much bigger and the rest of the body smaller
 
-# Beta 5 (Shoulder width):
-# Range: -3 to 3
-# Effect: -3 for narrower shoulders, 3 for wider shoulders
+# Beta 4 (Chest, Hip, and Abdomen Size):
+# Range: -5 to 5
+# Effect: -5 for smaller, +5 for larger chest, hips, and abdomen
 
-# Beta 6 (Hip width):
-# Range: -3 to 3
-# Effect: -3 for narrower hips, 3 for wider hips
+# Beta 5 (Big Belly with Overall Thinning):
+# Range: 0 to negative values
+# Effect: Negative values indicate a big belly with overall body thinning
 
-# Beta 7 (Chest depth):
-# Range: -3 to 3
-# Effect: -3 for shallower chest, 3 for deeper chest
+# Beta 6 (Disproportionate Belly Size):
+# Range: 0 to positive values
+# Effect: Positive values indicate a particularly large belly, while other parts of the body become very thin
 
-# Beta 8 (Waist size):
-# Range: -3 to 3
-# Effect: -3 for smaller waist, 3 for larger waist
+# Beta 7 (Vertical Body Compression):
+# Range: 0 to positive values
+# Effect: Positive values indicate vertical squeezing of the body
 
-# Beta 9 (Head size):
-# Range: -3 to 3
-# Effect: -3 for smaller head, 3 for larger head
+# Beta 8 (Horizontal Body Fattening):
+# Range: 0 to positive values
+# Effect: Positive values indicate horizontal fattening of the body
+
+# Beta 9 (Shoulder Width):
+# Range: 0 to positive values
+# Effect: Positive values indicate wider shoulders
+
 
 
 import os.path as osp
@@ -79,7 +81,7 @@ def main(model_folder,
          num_expression_coeffs=10,
          plotting_module='pyrender',
          use_face_contour=False,
-         save_path = 'output_model.obj'):
+         save_path = None):
 
     model = smplx.create(model_folder, model_type=model_type,
                          gender=gender, use_face_contour=use_face_contour,
@@ -90,16 +92,19 @@ def main(model_folder,
 
     betas, expression = None, None
 
-    if beta_values is not None:
-        betas = torch.tensor(beta_values, dtype=torch.float32).unsqueeze(0)       # Values for body shape parameters
+    print(beta_values)
 
-    if sample_shape:
+    if beta_values is not None:
+        betas = torch.tensor(beta_values, dtype=torch.float32)       # Values for body shape parameters
+    elif sample_shape:
         betas = torch.randn([1, model.num_betas], dtype=torch.float32)
+
+    
     if sample_expression:
         expression = torch.randn(
             [1, model.num_expression_coeffs], dtype=torch.float32)
         
-    # print(betas)
+    print(betas)
 
     output = model(betas=betas, expression=expression,
                    return_verts=True)
@@ -111,71 +116,72 @@ def main(model_folder,
 
     # Save the model
     # print(trimesh.__version__)
-    saved_model = trimesh.Trimesh(vertices, model.faces)
-    saved_model.export(save_path)
-    print(f"Model saved to {save_path}")
+    if save_path is not None:
+        saved_model = trimesh.Trimesh(vertices, model.faces)
+        saved_model.export(save_path)
+        print(f"Model saved to {save_path}")
 
 
-    if plotting_module == 'pyrender':
-        import pyrender
-        # import trimesh
-        vertex_colors = np.ones([vertices.shape[0], 4]) * [0.3, 0.3, 0.3, 0.8]
-        tri_mesh = trimesh.Trimesh(vertices, model.faces,
-                                   vertex_colors=vertex_colors)
+    # if plotting_module == 'pyrender':
+    #     import pyrender
+    #     # import trimesh
+    #     vertex_colors = np.ones([vertices.shape[0], 4]) * [0.3, 0.3, 0.3, 0.8]
+    #     tri_mesh = trimesh.Trimesh(vertices, model.faces,
+    #                                vertex_colors=vertex_colors)
 
-        mesh = pyrender.Mesh.from_trimesh(tri_mesh)
+    #     mesh = pyrender.Mesh.from_trimesh(tri_mesh)
 
-        scene = pyrender.Scene()
-        scene.add(mesh)
+    #     scene = pyrender.Scene()
+    #     scene.add(mesh)
 
-        if plot_joints:
-            sm = trimesh.creation.uv_sphere(radius=0.005)
-            sm.visual.vertex_colors = [0.9, 0.1, 0.1, 1.0]
-            tfs = np.tile(np.eye(4), (len(joints), 1, 1))
-            tfs[:, :3, 3] = joints
-            joints_pcl = pyrender.Mesh.from_trimesh(sm, poses=tfs)
-            scene.add(joints_pcl)
+    #     if plot_joints:
+    #         sm = trimesh.creation.uv_sphere(radius=0.005)
+    #         sm.visual.vertex_colors = [0.9, 0.1, 0.1, 1.0]
+    #         tfs = np.tile(np.eye(4), (len(joints), 1, 1))
+    #         tfs[:, :3, 3] = joints
+    #         joints_pcl = pyrender.Mesh.from_trimesh(sm, poses=tfs)
+    #         scene.add(joints_pcl)
 
-        pyrender.Viewer(scene, use_raymond_lighting=True)
-    elif plotting_module == 'matplotlib':
-        from matplotlib import pyplot as plt
-        from mpl_toolkits.mplot3d import Axes3D
-        from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+    #     pyrender.Viewer(scene, use_raymond_lighting=True)
+    # elif plotting_module == 'matplotlib':
+    #     from matplotlib import pyplot as plt
+    #     from mpl_toolkits.mplot3d import Axes3D
+    #     from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
+    #     fig = plt.figure()
+    #     ax = fig.add_subplot(111, projection='3d')
 
-        mesh = Poly3DCollection(vertices[model.faces], alpha=0.1)
-        face_color = (1.0, 1.0, 0.9)
-        edge_color = (0, 0, 0)
-        mesh.set_edgecolor(edge_color)
-        mesh.set_facecolor(face_color)
-        ax.add_collection3d(mesh)
-        ax.scatter(joints[:, 0], joints[:, 1], joints[:, 2], color='r')
+    #     mesh = Poly3DCollection(vertices[model.faces], alpha=0.1)
+    #     face_color = (1.0, 1.0, 0.9)
+    #     edge_color = (0, 0, 0)
+    #     mesh.set_edgecolor(edge_color)
+    #     mesh.set_facecolor(face_color)
+    #     ax.add_collection3d(mesh)
+    #     ax.scatter(joints[:, 0], joints[:, 1], joints[:, 2], color='r')
 
-        if plot_joints:
-            ax.scatter(joints[:, 0], joints[:, 1], joints[:, 2], alpha=0.1)
-        plt.show()
-    elif plotting_module == 'open3d':
-        import open3d as o3d
+    #     if plot_joints:
+    #         ax.scatter(joints[:, 0], joints[:, 1], joints[:, 2], alpha=0.1)
+    #     plt.show()
+    # elif plotting_module == 'open3d':
+    #     import open3d as o3d
 
-        mesh = o3d.geometry.TriangleMesh()
-        mesh.vertices = o3d.utility.Vector3dVector(
-            vertices)
-        mesh.triangles = o3d.utility.Vector3iVector(model.faces)
-        mesh.compute_vertex_normals()
-        mesh.paint_uniform_color([0.3, 0.3, 0.3])
+    #     mesh = o3d.geometry.TriangleMesh()
+    #     mesh.vertices = o3d.utility.Vector3dVector(
+    #         vertices)
+    #     mesh.triangles = o3d.utility.Vector3iVector(model.faces)
+    #     mesh.compute_vertex_normals()
+    #     mesh.paint_uniform_color([0.3, 0.3, 0.3])
 
-        geometry = [mesh]
-        if plot_joints:
-            joints_pcl = o3d.geometry.PointCloud()
-            joints_pcl.points = o3d.utility.Vector3dVector(joints)
-            joints_pcl.paint_uniform_color([0.7, 0.3, 0.3])
-            geometry.append(joints_pcl)
+    #     geometry = [mesh]
+    #     if plot_joints:
+    #         joints_pcl = o3d.geometry.PointCloud()
+    #         joints_pcl.points = o3d.utility.Vector3dVector(joints)
+    #         joints_pcl.paint_uniform_color([0.7, 0.3, 0.3])
+    #         geometry.append(joints_pcl)
 
-        o3d.visualization.draw_geometries(geometry)
-    else:
-        raise ValueError('Unknown plotting_module: {}'.format(plotting_module))
+    #     o3d.visualization.draw_geometries(geometry)
+    # else:
+    #     raise ValueError('Unknown plotting_module: {}'.format(plotting_module))
 
 
 if __name__ == '__main__':
@@ -219,7 +225,7 @@ if __name__ == '__main__':
                         type=lambda arg: arg.lower() in ['true', '1'],
                         help='Compute the contour of the face')
     
-    parser.add_argument('--save-path', default='output_model.obj', type=str,  # Add this argument
+    parser.add_argument('--save-path', default=None, type=str,  # Add this argument
                         help='The path to save the generated model')
 
     args = parser.parse_args()
@@ -245,6 +251,7 @@ if __name__ == '__main__':
     main(model_folder, model_type, ext=ext,
          gender=gender, plot_joints=plot_joints,
          num_betas=num_betas,
+         beta_values=beta_values,
          num_expression_coeffs=num_expression_coeffs,
          sample_shape=sample_shape,
          sample_expression=sample_expression,
